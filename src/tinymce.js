58f5d5bafc5e5dae6bdd1cafd43240ee9c8393c2
Dean Sofer <dsofer@paxata.com>
2013-05-16 16:16:49 -0700

    fix: Added ngModel.$render() and removed a need for an ID attribute, Fixes #6

--- src/tinymce.js
+++ src/tinymce.js
@@ -5,47 +5,52 @@ angular.module('ui.tinymce', [])
   .value('uiTinymceConfig', {})
   .directive('uiTinymce', ['uiTinymceConfig', function (uiTinymceConfig) {
     uiTinymceConfig = uiTinymceConfig || {};
+    var generatedIds = 0;
     return {
       require: 'ngModel',
       link: function (scope, elm, attrs, ngModel) {
-        var expression,
-          options = {
-            // Update model on button click
-            onchange_callback: function (inst) {
-              if (inst.isDirty()) {
-                inst.save();
+        var expression, options, tinyInstance;
+        // generate an ID if not present
+        if (!attrs.id) {
+          attrs.$set('id', 'uiTinymce' + generatedIds++);
+        }
+        options = {
+          // Update model on button click
+          onchange_callback: function (inst) {
+            if (inst.isDirty()) {
+              inst.save();
+              ngModel.$setViewValue(elm.val());
+              if (!scope.$$phase) {
+                scope.$apply();
+              }
+            }
+          },
+          // Update model on keypress
+          handle_event_callback: function (e) {
+            if (this.isDirty()) {
+              this.save();
+              ngModel.$setViewValue(elm.val());
+              if (!scope.$$phase) {
+                scope.$apply();
+              }
+            }
+            return true; // Continue handling
+          },
+          // Update model when calling setContent (such as from the source editor popup)
+          setup: function (ed) {
+            ed.onSetContent.add(function (ed, o) {
+              if (ed.isDirty()) {
+                ed.save();
                 ngModel.$setViewValue(elm.val());
                 if (!scope.$$phase) {
                   scope.$apply();
                 }
               }
-            },
-            // Update model on keypress
-            handle_event_callback: function (e) {
-              if (this.isDirty()) {
-                this.save();
-                ngModel.$setViewValue(elm.val());
-                if (!scope.$$phase) {
-                  scope.$apply();
-                }
-              }
-              return true; // Continue handling
-            },
-            // Update model when calling setContent (such as from the source editor popup)
-            setup: function (ed) {
-              ed.onSetContent.add(function (ed, o) {
-                if (ed.isDirty()) {
-                  ed.save();
-                  ngModel.$setViewValue(elm.val());
-                  if (!scope.$$phase) {
-                    scope.$apply();
-                  }
-                }
-              });
-            },
-            mode: 'exact',
-            elements: attrs.id
-          };
+            });
+          },
+          mode: 'exact',
+          elements: attrs.id
+        };
         if (attrs.uiTinymce) {
           expression = scope.$eval(attrs.uiTinymce);
         } else {
@@ -55,6 +60,12 @@ angular.module('ui.tinymce', [])
         setTimeout(function () {
           tinymce.init(options);
         });
+        ngModel.$render = function(){
+          if (!tinyInstance) {
+            tinyInstance = tinymce.get(attrs.id);
+          }
+          tinyInstance.setContent(ngModel.$viewValue);
+        };
       }
     };
   }]);
