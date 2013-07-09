f0bc078de4f39c8aca5f812ec97f93d7334cce61
Wesley Cho <wesley.cho@gmail.com>
2013-07-09 18:32:00 -0400

    Updated test.conf.js to reflect changes in default Bower install folder

--- test/test.conf.js
+++ test/test.conf.js
@@ -2,9 +2,9 @@ basePath = '..';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
-  'components/tinymce/tinymce.min.js',
+  'bower_components/angular/angular.js',
+  'bower_components/angular-mocks/angular-mocks.js',
+  'bower_components/tinymce/tinymce.min.js',
   'src/tinymce.js',
   'test/*.spec.js'
 ];
