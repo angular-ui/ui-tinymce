ba4079e38d4c757efe7a03e19382e369f593946a
Juan Ignacio Vásquez <juanin@MacBook-Pro-de-Juan.local>
2013-11-26 16:10:05 -0300

    Fixed bug with setup on options

--- src/tinymce.js
+++ src/tinymce.js
@@ -28,38 +28,39 @@ angular.module('ui.tinymce', [])
         }
         options = {
           // Update model when calling setContent (such as from the source editor popup)
-          setup: function (ed) {
-            var args;
-            ed.on('init', function(args) {
-              ngModel.$render();
-            });
-            // Update model on button click
-            ed.on('ExecCommand', function (e) {
-              ed.save();
-              updateView();
-            });
-            // Update model on keypress
-            ed.on('KeyUp', function (e) {
-              ed.save();
-              updateView();
-            });
-            // Update model on change, i.e. copy/pasted text, plugins altering content
-            ed.on('SetContent', function (e) {
-              if(!e.initial){
-                ed.save();
-                updateView();
-              }
-            });
-            if (expression.setup) {
-              scope.$eval(expression.setup);
-              delete expression.setup;
-            }
-          },
           mode: 'exact',
           elements: attrs.id
         };
         // extend options with initial uiTinymceConfig and options from directive attribute value
         angular.extend(options, uiTinymceConfig, expression);
+        // This should be called after doing angular.extend otherwise it gets replaced by the one that's inside express
+        options.setup = function (ed) {
+          var args;
+          ed.on('init', function(args) {
+            ngModel.$render();
+          });
+          // Update model on button click
+          ed.on('ExecCommand', function (e) {
+            ed.save();
+            updateView();
+          });
+          // Update model on keypress
+          ed.on('KeyUp', function (e) {
+            ed.save();
+            updateView();
+          });
+          // Update model on change, i.e. copy/pasted text, plugins altering content
+          ed.on('SetContent', function (e) {
+            if(!e.initial){
+              ed.save();
+              updateView();
+            }
+          });
+          if (expression.setup) {
+            expression.setup(ed);
+            delete expression.setup;
+          }
+        };
         setTimeout(function () {
           tinymce.init(options);
         });
