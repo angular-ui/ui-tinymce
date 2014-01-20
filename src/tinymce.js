82a09b9f9a8525c9f4d07966aff76d303700e68b
maxoumime <maxime.bertheau@hotmail.fr>
2014-01-20 15:30:07 +0100

    Fix Content not showing w/ Angular 1.2+ | Long time getting Content maxoumime

--- src/tinymce.js
+++ src/tinymce.js
@@ -7,6 +7,7 @@ angular.module('ui.tinymce', [])
     uiTinymceConfig = uiTinymceConfig || {};
     var generatedIds = 0;
     return {
+      priority: 1,
       require: 'ngModel',
       link: function (scope, elm, attrs, ngModel) {
         var expression, options, tinyInstance,
@@ -64,7 +65,17 @@ angular.module('ui.tinymce', [])
           tinymce.init(options);
         });
 
+        var stopWatch = scope.$watch(attrs.ngModel, function(newValue) {
+        	
+		    tinyInstance = tinymce.get(attrs.id);
 
+		    if(tinyInstance){
+		        tinyInstance.setContent(newValue);
+		        stopWatch();
+		    }
+		});
+
+        
         ngModel.$render = function() {
           if (!tinyInstance) {
             tinyInstance = tinymce.get(attrs.id);
