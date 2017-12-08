753d68535f002ebe098155c4e628c07e624e053d
Stefan <stefan.gencarau@avelon.ch>
2017-12-08 16:06:01 +0200

    Format

--- src/tinymce.js
+++ src/tinymce.js
@@ -199,7 +199,7 @@ angular.module('ui.tinymce', [])
           if (tinyInstance) {
             tinyInstance.remove();
             tinyInstance = null;
-	    options.setup = null;
+			options.setup = null;
           }
         });
 
